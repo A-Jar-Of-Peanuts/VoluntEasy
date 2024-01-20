eee02a0b642c6346a83ebd68040724763a8f3bc5
NehpetSS <stephen.hong@yahoo.ca>
2024-01-20 14:48:30 -0800

    components

---
+++ client/src/components/NavigationBar.js
@@ -0,0 +1,13 @@
+
+
+export default function NavigationBar() {
+    return (
+        <div className="NavigationBar">
+            <button>Home</button>
+            <div>
+                <button>Login</button>
+                <button>Register</button>
+            </div>
+        </div>
+    ) 
+}
