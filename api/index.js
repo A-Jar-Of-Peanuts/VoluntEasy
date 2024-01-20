dfaa8b0144f6debf6d00f85d647bca474e2029af
jlieu88 <j_lieu@outlook.com>
2024-01-20 15:05:08 -0800

    added user add functionality to mongo

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
-const express = require('express');
+const express = require('express');     
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
+const User = require('./models/User');
 const app = express();
 
 mongoose.connect("mongodb+srv://ajarofpeanuts:<Z$Ztroqy^Ns5h7yk>@cluster.zcwwmbg.mongodb.net/?retryWrites=true&w=majority")
@@ -9,8 +11,39 @@ app.get('/test', async (req,res) => {
   res.json('test ok');
 });
 
-app.post('')
+app.post('/register', async (req, res) => {
+  const{ username, password } = req.body;
+  try {
+    const user = await User.create({
+      username,
+      password
+    });
+    res.json(userDoc);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+})
 
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  
+  // Users must be unique
+  const user = await User.findOne({username});
+  const validPassword = password === user.password;
+
+  if(validPassword) {
+    jwt.sign({ username, id:user._id }, {}, (err,token) => {
+      if(err) throw err;
+      res.cookie('token', token).json({
+        id:user._id,
+        username,
+      })
+    })
+  } else {
+    res.status(400).json("Incorrect credentials");
+  }
+})
 
 
 
