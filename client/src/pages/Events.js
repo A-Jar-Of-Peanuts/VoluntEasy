11dca59f2accaff13cd61cb4e0fcd785033c9fa4
A-Jar-Of-Peanuts <ajarofpeanuts@gmail.com>
2024-01-20 15:07:16 -0800

    todo

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,8 +1,17 @@
 import Post from '../components/Post'
+import NavigationBar from '../components/NavigationBar'
 
 function Events() {
  return (
-    <Post title = "NWHacks" location = "UR MOM" description = "DOING YOUR MOM" eventTime='2024-01-20 03:01:50' />
+    <div>
+        <div>
+            <h2>VOLUNTEASY</h2>
+        </div>
+        <div>
+            <NavigationBar/>
+        </div>
+        <Post title = "NWHacks" location = "UR MOM" description = "DOING YOUR MOM" eventTime='2024-01-20 03:01:50' />
+    </div>
  );
 }
 export default Events;
