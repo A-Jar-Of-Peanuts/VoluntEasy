3b4312f7716670cd6679bf1991c902cc2b7e68ce
jlieu88 <j_lieu@outlook.com>
2024-01-20 14:23:29 -0800

    added connection to mongoose

--- api/index.js
+++ api/index.js
@@ -3,9 +3,16 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect("mongodb+srv://ajarofpeanuts:<Z$Ztroqy^Ns5h7yk>@cluster.zcwwmbg.mongodb.net/?retryWrites=true&w=majority")
+
 app.get('/test', async (req,res) => {
   res.json('test ok');
 });
+
+app.post('')
+
+
+
 
 
 app.listen(4000);
