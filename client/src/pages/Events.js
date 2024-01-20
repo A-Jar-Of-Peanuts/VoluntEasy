dcc1f324a2f16f57887bf1d6818a3456a1bbc963
A-Jar-Of-Peanuts <ajarofpeanuts@gmail.com>
2024-01-20 15:04:01 -0800

    events page

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,4 +1,8 @@
-function Events() {
+import Post from '../components/Post'
 
+function Events() {
+ return (
+    <Post title = "NWHacks" location = "UR MOM" description = "DOING YOUR MOM" eventTime='2024-01-20 03:01:50' />
+ );
 }
 export default Events;
