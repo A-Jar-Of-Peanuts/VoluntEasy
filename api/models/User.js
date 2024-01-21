435a44f42494510b3c2404bcc1707423f04a16d7
jlieu88 <j_lieu@outlook.com>
2024-01-21 02:36:04 -0800

    added user subscribed and created events

--- api/models/User.js
+++ api/models/User.js
@@ -5,6 +5,8 @@ const {Schema} = mongoose;
 const UserSchema = new Schema({
     username: {type: String, required: true, min: 4, unique: true},
     password: {type: String, required: true},
+    eventsCreated: [],
+    eventsSubscribed: [],
 });
 
 const UserModel = model('User', UserSchema);
